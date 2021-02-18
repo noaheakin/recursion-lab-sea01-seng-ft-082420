3d8711f378347e580c04280d824ca4322bafb080
noaheakin <noaheakin@gmail.com>
2021-02-17 22:13:49 -0800

    commit tree b9a73b1b088a18a9be3c1ce59cb99d648487a8fc

--- index.js
+++ index.js
@@ -1,2 +1,49 @@
-// Code your solution here!
+function printString(string) {
+    console.log(string[0])
+    if (string.length > 1) {
+        let remainder = string.slice(1, string.length)
+        printString(remainder)
+    }
+}
 
+function reverseString(string) {
+    if (string.length < 2) {
+        return string
+    } else {
+        return reverseString(string.substring(1)) + string[0]
+    }
+}
+
+function isPalindrome(string) {
+    if (string.length < 2) {
+        return true
+    } else if (string[0] === string[string.length - 1]) {
+        return isPalindrome(string.substring(1, string.length - 1))
+    } else {
+        return false
+    }
+}
+
+function addUpTo(arr, index) {
+    return index ? arr[index] + addUpTo(arr, (index - 1)) : arr[index]
+}
+
+function maxOf(arr) {
+    if (arr.length === 1) {
+        return arr[0]
+    } else {
+        return Math.max(arr.pop(), maxOf(arr))
+    }
+}
+
+function includesNumber(arr, target) {
+    if (arr.length > 0) {
+        if (arr[0] === target) {
+            return true
+        } else {
+            return includesNumber(arr.slice(1), target)
+        }
+    } else {
+        return false
+    }
+}
